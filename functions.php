a4cec66bf151a22169f8c92402ee5724ae0c0c78
Daniel Brown <fireflip@gmail.com>
2018-04-21 16:21:45 -0500

    Set fonts

--- functions.php
+++ functions.php
@@ -236,7 +236,7 @@ can replace these fonts, change it in your scss files
 and be up and running in seconds.
 */
 function starter_fonts() {
-  wp_enqueue_style('googleFonts', '//fonts.googleapis.com/css?family=Lora:400,400i|Roboto:300,300i,400,400i,500,700,900');
+  wp_enqueue_style('googleFonts', '//fonts.googleapis.com/css?family=Cutive+Mono|IBM+Plex+Mono:300,400,700|IBM+Plex+Sans:300,400,500,700');
 }
 
 add_action('wp_enqueue_scripts', 'starter_fonts');
